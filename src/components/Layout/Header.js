7d05ce9336c96d3673dfde8b6c4e4cc1e2088267
DavidsJulio <davidsjulio97@gmail.com>
2021-05-14 12:00:01 +0100

    Adding the Cart button Component; Fixing a bug in previous header

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -1,16 +1,17 @@
 import React, { Fragment } from "react";
 import classes from "./Header.module.css";
 import mealsImage from "../../assets/meals.jpg";
+import HeaderCartButton from "./HeaderCartButton";
 
 function Header(props) {
   return (
     <Fragment>
       <header className={classes.header}>
         <h1>React Meals</h1>
-        <button>Cart</button>
+        <HeaderCartButton />
       </header>
       <div className={classes["main-image"]}>
-        <img src={mealsImage} alt="Meals Image" />
+        <img src={mealsImage} alt="Meals" />
       </div>
     </Fragment>
   );
